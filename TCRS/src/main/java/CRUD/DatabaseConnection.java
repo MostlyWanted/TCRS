d8819fa0c94f0d14ca5105721791c191488518bb
BA23Algoma <146596093+BA23Algoma@users.noreply.github.com>
2024-03-13 05:49:00 -0400

    Sample Test classes for database
    Built some simple classes to test the sql database ability to connect and query

---
+++ TCRS/src/main/java/CRUD/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package CRUD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:h2:~/src/main/resources/trcs_script"; // Modify this URL based on your database configuration
+    private static final String USER = "tcrs"; // Modify this
+    private static final String PASSWORD = ""; // Modify this
+
+    public static void main(String[] args) {
+        try {
+            // Load the H2 JDBC driver
+            Class.forName("org.h2.Driver");
+
+            // Establish connection
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            // Use the connection to perform database operations
+            // Check if the connection is successful
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to connect to the database!");
+            }
+
+            // Close the connection
+            connection.close();
+
+            // Close the connection when done
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ TCRS/src/main/java/CRUD/SelectStatement.java
@@ -0,0 +1,49 @@
+package CRUD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectStatement {
+	public static void main(String[] args) {
+	    final String URL = "jdbc:h2:~/src/main/resources/trcs_script"; // Modify this URL based on your database configuration
+	    final String USER = "tcrs"; // Modify this
+	    final String PASSWORD = ""; // Modify this
+	    	    
+        try {
+            // Connect to the database
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            // Create a statement
+            Statement statement = connection.createStatement();
+            
+            // Execute a query
+           ResultSet resultSet = statement.executeQuery("SELECT * FROM TCRS.ACCOUNTS");
+
+            // Process the result set
+            while (resultSet.next()) {
+                 //Retrieve data by column index or name
+                int id = resultSet.getInt("accountid");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("passwordacc");
+                String firstName = resultSet.getString("firstname");
+                String lastName = resultSet.getString("lastname");
+                String agency = resultSet.getString("agency");
+                
+                System.out.println("ID " + id + " username: " + username + " password: " + password + " First Name: " + firstName + " Last Name: " + lastName + " agency: " + agency);
+            }
+
+           
+           resultSet.close();
+           statement.close();
+           connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	
+
+}
